11818327a21fde07ea30dc935181e95bab195f39
Dekaz <luankalil12@gmail.com>
2024-02-01 21:33:15 -0300

    Iniciando o projeto, reconhecimento de fala implementado

---
+++ main.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+import pyttsx3
+
+machine = pyttsx3.init()
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    voz = r.listen(source)
+    comando = r.recognize_google(voz, language='pt-BR').lower()
+    print(comando)
