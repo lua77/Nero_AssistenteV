ae264393bda1c377fcd027fe270cadbad50e8975
Dekaz <luankalil12@gmail.com>
2024-02-01 21:36:56 -0300

    voice ansewrs
    Pyttsx3 testado e funcionando com o basico

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ with sr.Microphone() as source:
     voz = r.listen(source)
     comando = r.recognize_google(voz, language='pt-BR').lower()
     print(comando)
+
+    machine.say(comando)
+    machine.runAndWait()
